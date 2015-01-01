7b01d6e1422d46e589d467348f0a4b12c693f841
Ubuntu <ubuntu@ip-172-31-43-170.eu-west-1.compute.internal>
2015-01-01 23:39:36 +0000

    ok

--- contacts/admin.py
+++ contacts/admin.py
@@ -27,22 +27,19 @@ class AccountInline(admin.TabularInline):
     extra = 0
 
 class PersonAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'get_groups')
+    list_display = ('first_name', 'last_name', 'groups')
     inlines = [AddressInline, EmailInline, PhoneInline, AccountInline, InfoInline, MemberInline]
-    
-    def get_groups(self, obj):
-        return "\n".join([grp.name for grp in obj.groups.all()])
-
-#class MemberAdmin(admin.ModelAdmin):
-#    filter_horizontal = ('groups',)
+    # Get the groups
+    def groups(self, obj):
+        return "\n".join([grp.name for grp in obj.member.groups.all()])
 
 # Register your models here.
 admin.site.register(Person, PersonAdmin)
-#admin.site.register(Member, MemberAdmin)
+#admin.site.register(Member)
 #admin.site.register(Info)
 #admin.site.register(Address)
-#admin.site.register(City)
-#admin.site.register(Country)
+admin.site.register(City)
+admin.site.register(Country)
 #admin.site.register(Email)
 #admin.site.register(Phone)
 #admin.site.register(Account)
